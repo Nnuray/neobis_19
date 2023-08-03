f05cfde07219ef13e7d1862863d4d3d0c489bd8e
Nuray <139881782+Nnuray@users.noreply.github.com>
2023-08-03 17:30:12 +0600

    Add files via upload

---
+++ week2/shop.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class shop {
+    private String name;
+    private int price;
+    private int amount;
+
+    public shop(String name, String price, int amount) { // constructor
+        this.name = name;
+        this.price = Integer.parseInt(price);
+        this.amount = amount;
+    }
+    public String getName() { //getter
+        return name;
+    }
+
+    public void setName(String name) { //setter
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price=price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount=amount;
+    }
+
+    public String toString() {
+        return "Уверены ли вы купить " + amount + " штук " + name + " за " + price*amount + " тг";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, amount);
+    }
+}
+
