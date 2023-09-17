74a5c6b2ca8cee146b966c3226bd6fb3d833f53d
Nuray <nuray.kavkaeva.04@gmail.com>
2023-09-17 17:19:13 +0600

    security update

--- week5/src/main/java/com/example/week4/Week4Application.java
+++ week5/src/main/java/com/example/week4/Week4Application.java
@@ -4,11 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Neobis project", version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "Neobis project", version = "1.0"), servers = {@Server(url = "/", description = "Default Server URL")})
 @SecurityScheme(
         name = "JWT",
         type = SecuritySchemeType.HTTP,
